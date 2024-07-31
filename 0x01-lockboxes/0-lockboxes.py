0a834e940a64b1e9039500c1573093d921e7fa54
otega07 <okiriotega@gmail.com>
2024-07-31 22:02:40 +0100

    initial commit

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -17,17 +17,11 @@ def canUnlockAll(boxes):
     n = len(boxes)
     seen_boxes = set([0])
     unseen_boxes = set(boxes[0]).difference(set([0]))
-    
-    while unseen_boxes:
+    while len(unseen_boxes) > 0:
         boxIdx = unseen_boxes.pop()
-        if boxIdx >= n or boxIdx < 0:
+        if not boxIdx or  boxIdx >= n or boxIdx < 0:
             continue
         if boxIdx not in seen_boxes:
-            unseen_boxes.update(boxes[boxIdx])
+            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
             seen_boxes.add(boxIdx)
-    
     return n == len(seen_boxes)
-
-
-if __name__ == '__main__':
-    main()
