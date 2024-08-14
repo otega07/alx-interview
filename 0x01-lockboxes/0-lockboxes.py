4ccc4b37f2152ea1569ab1fe417c4ba84756f417
otega07 <okiriotega@gmail.com>
2024-08-14 14:13:40 +0100

    third commit

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,24 +9,21 @@ def canUnlockAll(boxes):
     Determines if all boxes can be opened.
 
     Parameters:
-    boxes (List[List[int]]): The list of lists representing the boxes and their keys.
+    boxes (List[List[int]]): list of lists representing the boxes and the keys.
 
     Returns:
     bool: True if all boxes can be opened, else False.
     """
     n = len(boxes)
-    seen_boxes = {0}
+    seen_boxes = set([0])
     unseen_boxes = set(boxes[0]).difference([0])
 
     while unseen_boxes:
         boxIdx = unseen_boxes.pop()
-        if (
-            boxIdx >= n
-            or boxIdx < 0
-            or boxIdx in seen_boxes
-        ):
+        if (boxIdx >= n or boxIdx < 0 or
+                boxIdx in seen_boxes):  # Fixed indentation
             continue
         unseen_boxes.update(boxes[boxIdx])
         seen_boxes.add(boxIdx)
 
-    return len(seen_boxes)
+    return len(seen_boxes) == n
