71185f6bc5625288587d5795848dd53129645ad8
otega07 <okiriotega@gmail.com>
2024-09-14 10:16:56 +0100

    initial commit

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""2D matrix rotation
+"""
+def rotate_2d_matrix(matrix):
+    """Rotates a 2D matrix 90 degrees clockwise in-place."""
+
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    
+    for i in range(n):
+        matrix[i].reverse()
