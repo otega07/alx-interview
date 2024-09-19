4486f51736f6d802c8e8de97580f1d4926e59268
otega07 <okiriotega@gmail.com>
2024-09-19 07:44:45 +0100

    greed

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+""" Contains makeChange function"""
+
+
+def makeChange(coins, total):
+    """
+    Returns: fewest number of coins needed to meet total
+        If total is 0 or less, return 0
+        If total cannot be met by any number of coins you have, return -1
+    """
+    if total <= 0:
+        return 0
+
+    # Sort the coins in descending order once
+    coins.sort(reverse=True)
+    change = 0
+
+    # Iterate through each coin and use as many as possible
+    for coin in coins:
+        if coin <= total:
+            num_of_coins = total // coin
+            change += num_of_coins
+            total -= coin * num_of_coins
+
+        if total == 0:
+            return change
+
+    # If the loop ends and total is not zero, it's not possible to make the exact amount
+    return -1
